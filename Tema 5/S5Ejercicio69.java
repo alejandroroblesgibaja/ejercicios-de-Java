a0e00d04de7f0490bc056d40d369b083ad8d1533
Alejandro Robles Gibaja <ale.robles11.s@gmail.com>
2019-10-21 08:47:08 +0200

    Subiendo ejercicio 69

---
+++ Tema 5/S5Ejercicio69.java
@@ -0,0 +1,41 @@
+/**
+ * Ejercicios Java Tema 5
+ *
+ * @author Alejandro Robles Gibaja
+ */
+public class S5Ejercicio69 {
+  public static void main(String[] args) {
+    System.out.print("Introduzca la altura de la pirámide: ");
+    int altura = Integer.parseInt(System.console().readLine());
+    
+    int caracteresPorFila = 1;
+    int espaciosDelanteros = altura - 1;
+    
+      for (int i = 0; i < altura; i++) {
+        //Espacios delanteros cada fila
+        for (int j = 0; j < espaciosDelanteros; j++) {
+        System.out.print(" ");
+        }
+        //Pinta el flanco izquierdo 
+        for (int j = 0; j < caracteresPorFila; j++) {
+        System.out.print("*");
+        }
+        
+        //Pinta la parte central
+        if (i % 2 == 0) { //cuando i es par
+          System.out.print("****");
+        } else {
+          System.out.print("    ");
+        }
+        
+        //Pinta el flanco derecho 
+        for (int j = 0; j < caracteresPorFila; j++) {
+        System.out.print("*");
+        }
+        System.out.println();
+      
+        caracteresPorFila ++;
+        espaciosDelanteros --;
+    }
+  }
+}
